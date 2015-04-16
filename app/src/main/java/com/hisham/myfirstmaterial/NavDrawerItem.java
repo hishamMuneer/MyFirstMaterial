52f737c18aad695248aba2e687967f584fc83ac3
Hisham <hisham.dbd@gmail.com>
2015-04-16 16:36:56 +0530

    Initial commit :boom:

---
+++ app/src/main/java/com/hisham/myfirstmaterial/NavDrawerItem.java
@@ -0,0 +1,35 @@
+package com.hisham.myfirstmaterial;
+
+/**
+ * Created by NI-PC1 on 4/16/2015.
+ */
+public class NavDrawerItem {
+    private boolean showNotify;
+    private String title;
+
+
+    public NavDrawerItem() {
+
+    }
+
+    public NavDrawerItem(boolean showNotify, String title) {
+        this.showNotify = showNotify;
+        this.title = title;
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
